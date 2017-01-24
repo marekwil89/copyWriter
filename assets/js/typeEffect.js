9491a0633167becf602a1565d298384e6c48bfc4
marekwil89 <marekwil89@gmail.com>
2017-01-24 18:41:49 +0100

    init & header section

---
+++ assets/js/typeEffect.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+  $(".header-type").typed({
+    strings: ["Doskonale władam słowem", "Gram w hokej", "Latam i koloryzuje", "Doskonale władam słowem, Gram w hokej, Latam i koloryzuje"],
+    typeSpeed: 2
+  });
+});
