980c3e1d112e7985029af36c8d7cdcacdb2b4253
marekwil89 <marekwil89@gmail.com>
2017-01-25 23:22:12 +0100

    typeEffect init & info-section added

--- assets/js/typeEffect.js
+++ assets/js/typeEffect.js
@@ -1,6 +1,23 @@
 $( document ).ready(function() {
-  $(".header-type").typed({
-    strings: ["Doskonale władam słowem", "Gram w hokej", "Latam i koloryzuje", "Doskonale władam słowem, Gram w hokej, Latam i koloryzuje"],
-    typeSpeed: 2
-  });
+  var headerSentences = ["władam słowem", "Gram w hokej", "Latam i koloryzuje", "władam słowem, Gram w hokej, Latam i koloryzuje"]
+
+  function writeEffect(sentences, element, typeSpeed){
+    var element = $(element)
+    i = 0
+    j = 0
+
+    var typing = setInterval(function (){
+      i++
+      if(i > sentences[j].length+5){
+        j++
+        if(j >= sentences.length){
+          clearInterval(typing)
+        }
+        i = 0
+      }
+     element.text(sentences[j].slice(0, i));
+    }, typeSpeed);  
+  }
+
+  writeEffect(headerSentences, '.header-typing', 70)
 });
