fd8e7b01eed00c60056c757b6fdcb502babbb0ee
marekwil89 <marekwil89@gmail.com>
2017-01-29 23:06:18 +0100

    loader, nav upgrade, logo, anchor scroll

--- assets/js/mobileMenu.js
+++ assets/js/mobileMenu.js
@@ -1,18 +1,41 @@
 $( document ).ready(function() {
+  anchorScroll();
+  transparentMenu();
+
+  $('.fa-bars').on('click', toggleMenu)
   $(window).scroll(function(){
-
-    var wScroll = $(this).scrollTop();
-    if(wScroll > 0){
-      $('nav').removeClass('transparent')
-    }
-    else{
-      $('nav').addClass('transparent')
-    }
-
+    transparentMenu()
   });
 
-  $('.fa-bars').on('click', function(){
-    $('.menu').toggleClass('opened');
-  })
-
 });
+
+function transparentMenu(){
+  var wScroll = $(this).scrollTop();
+  if(wScroll > 0){
+    $('nav').removeClass('transparent')
+  }
+  else{
+    $('nav').addClass('transparent')
+  }
+}
+
+function toggleMenu(){
+  $('.menu').toggleClass('opened');
+}
+
+function anchorScroll(){
+  $('a[href*="#"]:not([href="#"])').click(function(){
+  if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
+
+    var target=$(this.hash);
+    toggleMenu()
+    target = target.length ? target:$('[name='+this.hash.slice(1)+']');
+
+    if(target.length){$('html, body').animate({
+      scrollTop:target.offset().top
+    },1000);
+
+      return false;
+    }}
+  });
+}
