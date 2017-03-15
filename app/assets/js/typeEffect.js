0036b7b071d2f19fb7f0af22f30feedd79c356b2
marekwil89 <marekwil89@gmail.com>
2017-03-15 22:14:16 +0100

    changes v10

--- app/assets/js/typeEffect.js
+++ app/assets/js/typeEffect.js
@@ -1,23 +1,23 @@
-// $( document ).ready(function() {
-//   var headerSentences = ["Konkretny Content Doskonale gotuję władam słowem, latam i koloryzuję"]
+$( document ).ready(function() {
+  var headerSentences = ["Napisz swój sukces"]
 
-//   writeEffect(headerSentences, '.header-typing', 70)
-// });
+  writeEffect(headerSentences, '.type', 70)
+});
 
-// function writeEffect(sentences, element, typeSpeed){
-//   var element = $(element)
-//   i = 0
-//   j = 0
+function writeEffect(sentences, element, typeSpeed){
+  var element = $(element)
+  i = 0
+  j = 0
 
-//   var typing = setInterval(function (){
-//     i++
-//     if(i > sentences[j].length){
-//       j++
-//       if(j >= sentences.length){
-//         return clearInterval(typing)
-//       }
-//       i = 0
-//     }
-//    element.text(sentences[j].slice(0, i));
-//   }, typeSpeed);  
-// }
+  var typing = setInterval(function (){
+    i++
+    if(i > sentences[j].length){
+      j++
+      if(j >= sentences.length){
+        return clearInterval(typing)
+      }
+      i = 0
+    }
+   element.text(sentences[j].slice(0, i));
+  }, typeSpeed);  
+}
